9ad0fc2beca32c5c7ddc87461b123c4df5284eb1
Ankit Jain <ankit.xavierite@gmail.com>
2020-05-27 19:37:39 +0530

    changed the folder structure.

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod", package = None)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "to_do_list.settings", package = None)
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
